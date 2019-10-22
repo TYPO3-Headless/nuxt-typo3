76e66e00059ab556782ce6005016901fb1eb8b87
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2019-10-22 17:20:28 +0200

    feat(ce): add bullets CE

---
+++ lib/templates/components/content/elements/CeBullets.vue
@@ -0,0 +1,42 @@
+<template functional>
+  <div class="ce-bullets">
+    <ce-header v-bind="props" />
+    <ul
+      :is="props.bulletsType === 1 ? 'ol' : 'ul'"
+      v-if="props.bulletsType === 0 || props.bulletsType === 1"
+    >
+      <li v-for="(el, index) in props.bodytext" :key="index">
+        {{ el }}
+      </li>
+    </ul>
+    <dl v-else>
+      <template v-for="(el, index) in props.bodytext">
+        <dt :key="index">
+          {{ el[0] }}
+        </dt>
+        <dd v-if="el[1]" :key="index">
+          {{ el[1] }}
+        </dd>
+      </template>
+    </dl>
+  </div>
+</template>
+<script>
+import baseCe from '../mixins/baseCe'
+export default {
+  name: 'CeBullets',
+  extends: baseCe,
+  props: {
+    bodytext: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    bulletsType: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  }
+}
+</script>
