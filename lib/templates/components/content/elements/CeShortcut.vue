368c531c519b30e97c255465b1f19fdcaca8d7ab
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2019-10-22 18:47:58 +0200

    feat(ce): add shortcut CE

---
+++ lib/templates/components/content/elements/CeShortcut.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'CeShortcut',
+  functional: true,
+  props: {
+    shortcut: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+  render(createElement, ctx) {
+    return createElement('div', [
+      createElement('ce-renderer', {
+        props: {
+          content: ctx.props.shortcut
+        }
+      })
+    ])
+  }
+}
+</script>
