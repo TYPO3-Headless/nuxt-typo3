d5d664112cdff742ad833e2b6ee67e2dfb84ddc3
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2019-10-22 18:15:57 +0200

    feat(ce): add HTML CE

---
+++ lib/templates/components/content/elements/CeHtml.vue
@@ -0,0 +1,20 @@
+<template functional>
+  <div class="ce-html">
+    <html-parser :content="props.bodytext" />
+  </div>
+</template>
+<script>
+import baseCe from '../mixins/baseCe'
+export default {
+  name: 'CeHtml',
+  functional: true,
+  extends: baseCe,
+  props: {
+    bodytext: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
