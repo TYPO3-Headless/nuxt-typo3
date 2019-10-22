462b11e2f1345e0a1cc293aebb41e0327b9a30dd
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2019-10-22 14:19:43 +0200

    feat(ce): add textpic CE

---
+++ lib/templates/components/content/elements/CeTextpic.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ce-textpic">
+    <media-gallery :gallery="image.gallery" :files="image.images">
+      <ce-header v-bind="$props" />
+      <html-parser :content="bodytext" />
+    </media-gallery>
+  </div>
+</template>
+<script>
+import baseCe from '../mixins/baseCe'
+import MediaGallery from './media/Gallery'
+export default {
+  name: 'CeTextpic',
+  components: {
+    MediaGallery
+  },
+  extends: baseCe,
+  props: {
+    image: {
+      type: Object,
+      required: true,
+      default: () => {}
+    },
+    bodytext: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
