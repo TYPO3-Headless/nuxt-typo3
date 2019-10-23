e25c5093323ec526c57291b6309b48af5bffe0a5
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2019-10-23 18:03:04 +0200

    feat(ce): add textmedia ce

--- example/plugins/components.js
+++ example/plugins/components.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import CeText from '~/components/content/elements/CeText'
-import CeTextmedia from '~/components/content/elements/CeTextmedia'
 
 const components = {
-  CeText,
-  CeTextmedia
+  CeText
 }
 
 export default ({ app }) => {
---
+++ lib/templates/components/content/elements/CeTextmedia.vue
@@ -0,0 +1,13 @@
+<template>
+  <textpic v-bind="$props" class="ce-textmedia" />
+</template>
+<script>
+import Textpic from './CeTextpic'
+export default {
+  name: 'CeTextmedia',
+  components: {
+    Textpic
+  },
+  extends: Textpic
+}
+</script>
