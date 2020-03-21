68527a612a05128c2800cf13ada2135b0bd38589
mercs600 <kontakt@mercs600.pl>
2020-03-21 19:40:54 +0100

    feat(layouts): add page content for error pages (#19)

--- example/layouts/error.vue
+++ example/layouts/error.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>ERROR PAGE {{ error.statusCode }}</h1>
+    <be-dynamic
+      v-if="error.ssr"
+      :page="error.ssr.pageContent.page"
+      :content="error.ssr.pageContent.content"
+      :type="error.ssr.backendLayout"
+    />
   </div>
 </template>
 <script>
