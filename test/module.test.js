c347c6f3eb6ed079ba866f12bfea14740c3f4e61
mercs600 <kontakt@mercs600.pl>
2020-07-08 19:45:05 +0200

    chore(deps): remove unnecessary deps

--- test/module.test.js
+++ test/module.test.js
@@ -1,27 +1,19 @@
-jest.setTimeout(60000)
+const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
 
-const { Nuxt, Builder } = require('nuxt-edge')
+describe('module', () => {
+  let nuxt
 
-const config = require('../example/nuxt.config')
-// config.dev = false
-
-let nuxt
-
-describe('basic', () => {
   beforeAll(async () => {
-    nuxt = new Nuxt(config)
-    await nuxt.ready()
-    await new Builder(nuxt).build()
-    await nuxt.server.listen(3000, 'localhost')
-  })
+    ;({ nuxt } = await setup(loadConfig(__dirname, '../../example')))
+  }, 60000)
 
   afterAll(async () => {
     await nuxt.close()
   })
 
   test('render', async () => {
-    const context = {}
-    const { html } = await nuxt.server.renderRoute('/', context)
+    // const context = {}
+    const html = await get('/')
     expect(html).toContain('page')
   })
 })
