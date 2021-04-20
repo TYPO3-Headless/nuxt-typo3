9d8f6a58df7eb22cc68492fce45bf2635e55b989
Marcin Górski <martinmountie@gmail.com>
2021-04-21 01:27:55 +0200

    fix: correct wrong component tag name

--- lib/templates/components/content/elements/CeHeader.vue
+++ lib/templates/components/content/elements/CeHeader.vue
@@ -5,9 +5,9 @@
       v-if="headerLayout >= 0 && headerLayout !== 100"
       :class="headerPosition"
     >
-      <nav-link v-if="headerLink" :to="headerLink.url">
+      <nuxt-link v-if="headerLink" :to="headerLink.url">
         {{ header }}
-      </nav-link>
+      </nuxt-link>
       <template v-else>{{ header }}</template>
     </component>
     <component :is="`h${headerLevel + 1}`" v-if="subheader">
