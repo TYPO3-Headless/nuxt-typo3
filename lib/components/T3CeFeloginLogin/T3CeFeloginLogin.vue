1ab237db9b2f5010ec1ce99e4f17299e0f81eeb8
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-11-29 20:04:20 +0100

    feat(component): create basic fe-login component

---
+++ lib/components/T3CeFeloginLogin/T3CeFeloginLogin.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fe-login">
+    <T3Form ref="form" :elements="elements" @submit.prevent="onSubmit" />
+  </div>
+</template>
+<script>
+import T3Form from '../../components/T3Form/T3Form'
+import baseCe from '../../mixins/component/baseCe'
+
+export default {
+  name: 'T3CeFeloginLogin',
+  components: {
+    T3Form
+  },
+  extends: baseCe,
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    elements () {
+      return this.data.form.fields.pages
+    }
+  },
+  methods: {
+    onSubmit () {
+      alert('submitted')
+    }
+  }
+}
+</script>
