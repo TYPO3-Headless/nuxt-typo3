31ab5efc333d9b2594034ddc7e1a3063eace119c
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-12-14 19:59:48 +0100

    feat(fe-login): display form messages above the form

--- lib/components/T3CeFeloginLogin/T3CeFeloginLogin.vue
+++ lib/components/T3CeFeloginLogin/T3CeFeloginLogin.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="fe-login">
+    <p>{{ $props.data.message.header }}</p>
+    <p>{{ $props.data.message.message }}</p>
     <T3Form ref="form" :elements="elements" @submit="onSubmit" />
   </div>
 </template>
