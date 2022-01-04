ce764a07a089de6f12ed4154738e59bd8909d16a
Adam <kontakt@mercs600.pl>
2022-01-04 17:07:00 +0100

    feat(fe-login): add felogin setup
    * feat(component): create basic fe-login component
    
    * feat(forms): map type=hidden to T3FormFieldHidden
    
    * feat(form): map type=hidden to T3FormFieldHidden
    
    * feat: remove debug output and add basic http request onSubmit
    
    * fix(fe-login): tell api to only send data for the current element
    
    * feat(fe-login): add very basic transformation of form elements
    
    * feat(fe-login): display form messages above the form
    
    * feat(fe-login): reload initial data after successfull login
    
    * feat(fe-login): redirect the user to the redirect_url, if provided
    
    * chore(fe-login): fix typo
    
    * chore(fe-login): move compilation of form elements into its own function
    
    * fix(fe-login): we don't need to do anything to element.validators anymore
    
    * chore(fe-login): remove unneeded $props
    
    * feat(fe-login): filter out the submit button
    
    We use the T3Form cta and don't need the submit button
    
    * feat(fe-login): add element.name as id to form elements
    
    * fix(fe-login): use redirectUrl from the response instead of the form
    
    * refactor: use async-await for login, validation and separate methods
    
    * feat: add felogin logged in state
    
    Co-authored-by: Johannes Przymusinski <johannes.przymusinski@jop-software.de>

---
+++ lib/components/T3CeFeloginLogin/T3CeFeloginLogin.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="fe-login">
+    <!-- eslint-disable vue/no-v-html -->
+    <p v-html="message.header" />
+    <p v-html="message.message" />
+    <!-- eslint-enable vue/no-v-html -->
+
+    <T3Form ref="form" :elements="elements" @submit="onSubmit">
+      <template #cta>
+        <button type="submit">
+          {{ loginType === 'login' ? 'Log in' : 'Log out' }}
+        </button>
+      </template>
+    </T3Form>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+import T3Form from '../../components/T3Form/T3Form'
+import baseCe from '../../mixins/component/baseCe'
+
+export default {
+  name: 'T3CeFeloginLogin',
+  components: {
+    T3Form
+  },
+  extends: baseCe,
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      message: this.data.message
+    }
+  },
+  computed: {
+    ...mapState({
+      isLoggedIn: state => state.typo3.isLoggedIn
+    }),
+    elements () {
+      return this.compileFormElements(this.data.form.elements || this.data.form.fields)
+    },
+    loginType () {
+      return (this.data.form.elements || this.data.form.fields)?.find(el => el.name === 'logintype')?.value
+    }
+  },
+  methods: {
+    compileFormElements (elements) {
+      return elements
+        .filter(element => element.type !== 'submit')
+        .map(element => ({
+          value: element.value || '',
+          type: element.type || '',
+          identifier: element.name,
+          label: element.label || '',
+          placeholder: element.placeholder || '',
+          name: element.name || '',
+          validators: element.validators
+        }))
+    },
+    async onSubmit ({ validator, form, formRef }) {
+      const { flags } = validator
+      const data = new FormData(formRef)
+      data.append('responseElementId', this.id.toString())
+      data.append('responseElementRecursive', '1')
+
+      if (flags.valid) {
+        try {
+          const response = await this.$typo3.api.$http.$post(this.data.form.action, data)
+          const { redirectUrl, status } = response.content.data
+
+          if (status === 'success') {
+            try {
+              await this.$store.dispatch('getInitialData', { path: redirectUrl === '/' ? '' : redirectUrl || this.$route.path })
+              this.$store.commit('SET_USER_STATE', this.loginType === 'login') // should be setup by API in initialData
+            } finally {
+              this.onSuccess(response, this.loginType)
+            }
+          } else {
+            throw response
+          }
+        } catch (error) {
+          this.onFailure(error)
+        }
+      }
+    },
+    onSuccess (response, loginType = this.loginType) {
+      const { redirectUrl } = response.content.data
+
+      if (redirectUrl) {
+        this.$router.push(redirectUrl)
+      } else if (loginType === 'logout') {
+        this.$router.push('/')
+      }
+    },
+    onFailure (response) {
+      const { message } = response?.content?.data
+
+      if (message) {
+        this.message = message
+      }
+    }
+  }
+}
+</script>
