e498135a5c013f50d58a64a4fa28fea3825708be
Adam <a.marcinkowski@macopedia.com>
2023-01-10 14:53:16 +0100

    ci: generate docs pages

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,5 +2,8 @@ import { createResolver, logger } from '@nuxt/kit'
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt-typo3/'
+  },
   extends: '@nuxt-themes/docus'
 })
